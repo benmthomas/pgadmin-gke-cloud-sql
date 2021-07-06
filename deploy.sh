94bbb445bcd2b0539347a4126e79876268f3895d
Ben <ben.thomas@deakin.edu.au>
2021-07-07 02:53:49 +1000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -13,7 +13,7 @@ set -o pipefail
 # Create the database credentials
 # kubectl apply -f ./kubernetes/gke/secret.yml
 
-# Create the secret that includes the user/pass for postgres
+# Create the secret that includes the user/pass of postgres instance
 echo 'Creating the postgres db secret'
 POSTGRES_USER="$(cd terraform && terraform output --raw postgres_user)"
 POSTGRES_PASS="$(cd terraform && terraform output --raw postgres_pass)"
@@ -22,14 +22,14 @@ kubectl create secret generic postgres-credentials \
   --from-literal=password="${POSTGRES_PASS}" \
   --dry-run=client -o yaml | kubectl apply -f -
 
-# Create the configmap that includes the connection string for the DB.
+# Create the configmap that includes the connection string to the postgres instance.
 echo 'Creating the postgresql connenction string Configmap'
 POSTGRES_CONNECTION="$(cd terraform && terraform output --raw postgres_instance_connection_name)"
 kubectl create configmap connectionname \
   --from-literal=connectionname="${POSTGRES_CONNECTION}" \
   --dry-run=client -o yaml | kubectl apply -f -
 
-# Create the service account
+# Create the K8s Service Account (KSA)
 kubectl create serviceaccount postgres-ksa -n default \
   --dry-run=client -o yaml | kubectl apply -f -
 
